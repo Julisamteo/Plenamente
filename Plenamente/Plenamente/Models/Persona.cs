ceb904549e6ffafd330cc576998078654e00d356
Carlos <Carlos@DESKTOP-213EP4J>
2019-04-10 09:55:21 -0500

    Correccion Foreign

--- Plenamente/Plenamente/Models/Persona.cs
+++ Plenamente/Plenamente/Models/Persona.cs
@@ -28,64 +28,48 @@ namespace Plenamente.Models
 
         [ForeignKey("TipoDocumento")]
         public int Tdoc_Id { get; set; }
+        public TipoDocumento TipoDocumento { get; set; }
         [ForeignKey("SedeCiudad")]
         public int Sciu_Id { get; set; }
+        public SedeCiudad SedeCiudad { get; set; }
         [ForeignKey("Ciudad")]
         public int Ciud_Id { get; set; }
+        public  Ciudad Ciudad { get; set; }
         [ForeignKey("CargoEmpresa")]
         public int Cemp_Id { get; set; }
+        public CargoEmpresa CargoEmpresa { get; set; }
         [ForeignKey("AreaEmpresa")]
         public int Aemp_Id { get; set; }
+        public AreaEmpresa AreaEmpresa { get; set; }
         [ForeignKey("CateLicencia")]
         public int Cate_Id { get; set; }
+        public CateLicencia CateLicencia { get; set; }
         [ForeignKey("Persona")]
         public int Id { get; set; }
+        public IdentityUser IdentityUser { get; set; }
         [ForeignKey("Genero")]
         public int Gene_Id { get; set; }
+        public  Genero Genero { get; set; }
         [ForeignKey("JornadaEmpresa")]
         public int Jemp_Id { get; set; }
+        public JornadaEmpresa JornadaEmpresa { get; set; }
         [ForeignKey("TipoVinculacion")]
         public int Tvin_Id { get; set; }
+        public TipoVinculacion TipoVinculacion { get; set; }
         [ForeignKey("EPS")]
         public int Eps_Id { get; set; }
+        public  EPS EPS { get; set; }
         [ForeignKey("AFP")]
         public int Afp_Id { get; set; }
+        public  AFP AFP { get; set; }
         [ForeignKey("ARL")]
         public int Arl_Id { get; set; }
+        public  ARL ARL { get; set; }
         [ForeignKey("Empresa")]
         public int Empr_Nit { get; set; }
+        public  Empresa Empresa { get; set; }
         [ForeignKey("EstadoPersona")]
         public int Espe_Id { get; set; }
-
-        [ForeignKey("Tdoc_Id")]
-        public virtual TipoDocumento TipoDocumento { get; set; }
-        [ForeignKey("Sciu_Id")]
-        public virtual SedeCiudad SedeCiudad { get; set; }
-        [ForeignKey("Ciud_Id")]
-        public virtual Ciudad Ciudad { get; set; }
-        [ForeignKey("Cemp_Id")]
-        public virtual CargoEmpresa CargoEmpresa { get; set; }
-        [ForeignKey("Aemp_Id")]
-        public virtual AreaEmpresa AreaEmpresa { get; set; }
-        [ForeignKey("Cate_Id")]
-        public virtual CateLicencia CateLicencia { get; set; }
-        [ForeignKey("Id")]
-        public virtual IdentityUser IdentityUser { get; set; }
-        [ForeignKey("Gene_Id")]
-        public virtual Genero Genero { get; set; }
-        [ForeignKey("Jemp_Id")]
-        public virtual JornadaEmpresa JornadaEmpresa { get; set; }
-        [ForeignKey("Tvin_Id")]
-        public virtual TipoVinculacion TipoVinculacion { get; set; }
-        [ForeignKey("Eps_Id")]
-        public virtual EPS EPS { get; set; }
-        [ForeignKey("Afp_Id")]
-        public virtual AFP AFP { get; set; }
-        [ForeignKey("Arl_Id")]
-        public virtual ARL ARL { get; set; }
-        [ForeignKey("Empr_Nit")]
-        public virtual Empresa Empresa { get; set; }
-        [ForeignKey("Espe_Id")]
-        public virtual EstadoPersona EstadoPersona { get; set; }        
+        public virtual EstadoPersona EstadoPersona { get; set; }
     }
 }
