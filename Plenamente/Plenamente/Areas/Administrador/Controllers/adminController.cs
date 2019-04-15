a160374020da38fa0ae8dd8a856cb7d236146630
Miguel <miguel.clavijo@contentgrp.com>
2019-04-15 14:54:43 -0500

    Actualización

---
+++ Plenamente/Plenamente/Areas/Administrador/Controllers/adminController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plenamente.Areas.Administrador.Controllers
+{
+    public class adminController : Controller
+    {
+        // GET: Administrador/admin
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
