068e184ec82f6d7ce70773fda35079de47293ed3
Miguel <miguel.clavijo@contentgrp.com>
2019-04-16 13:01:36 -0500

    Paginacion Y busqueda

--- Plenamente/Plenamente/Areas/Administrador/Controllers/CargoEmpresasController.cs
+++ Plenamente/Plenamente/Areas/Administrador/Controllers/CargoEmpresasController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using PagedList;
 using Plenamente.Models;
 
 namespace Plenamente.Areas.Administrador.Controllers
@@ -15,10 +16,43 @@ namespace Plenamente.Areas.Administrador.Controllers
         private ApplicationDbContext db = new ApplicationDbContext();
 
         // GET: Administrador/CargoEmpresas
-        public ActionResult Index()
+        public ViewResult Index(string sortOrder, string currentFilter, string searchString, int? page)
         {
-            var tb_CargoEmpresa = db.Tb_CargoEmpresa.Include(c => c.Empresa);
-            return View(tb_CargoEmpresa.ToList());
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
+
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+
+            ViewBag.CurrentFilter = searchString;
+
+            var cargos = from s in db.Tb_CargoEmpresa
+                           select s;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                cargos = cargos.Where(s => s.Cemp_Nom.Contains(searchString)
+                                       || s.Cemp_Nom.Contains(searchString));
+            }
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    cargos = cargos.OrderByDescending(s => s.Cemp_Nom);
+                    break;
+                default:  // Name ascending 
+                    cargos = cargos.OrderBy(s => s.Cemp_Nom);
+                    break;
+            }
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(cargos.ToPagedList(pageNumber, pageSize));
         }
 
         // GET: Administrador/CargoEmpresas/Details/5
