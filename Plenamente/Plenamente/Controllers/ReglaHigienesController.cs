cfe60d81c08aa768c982d954771f0ef4ea63cacf
Loko24 <epastor@sanmateo.edu.co>
2019-05-28 13:53:53 -0500

    Adding Company Filter

--- Plenamente/Plenamente/Controllers/ReglaHigienesController.cs
+++ Plenamente/Plenamente/Controllers/ReglaHigienesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 using PagedList;
 using Plenamente.Models;
 
@@ -32,9 +33,12 @@ namespace Plenamente.Areas.Administrador.Controllers
             }
 
             ViewBag.CurrentFilter = searchString;
-
+            var userId = User.Identity.GetUserId();
+            var UserCurrent = db.Users.Find(userId);
+            var Empr_Nit = UserCurrent.Empr_Nit;
             var rHigiene = from s in db.Tb_ReglaHigiene
-                         select s;
+                           where s.Empr_Nit == Empr_Nit
+                           select s;
             if (!String.IsNullOrEmpty(searchString))
             {
                 rHigiene = rHigiene.Where(s => s.Rhig_Registro.ToString().Contains(searchString)
