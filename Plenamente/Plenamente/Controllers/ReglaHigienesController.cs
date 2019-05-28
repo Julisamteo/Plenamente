e3dc471de83fb8cc990965c282e431267e0558bc
Carlos <Carlos@DESKTOP-213EP4J>
2019-05-28 13:11:01 -0500

    All U&D

--- Plenamente/Plenamente/Controllers/ReglaHigienesController.cs
+++ Plenamente/Plenamente/Controllers/ReglaHigienesController.cs
@@ -2,60 +2,28 @@
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
-using PagedList;
 using Plenamente.Models;
+using Plenamente.Models.ViewModel;
 
-namespace Plenamente.Areas.Administrador.Controllers
+namespace Plenamente.Controllers
 {
     public class ReglaHigienesController : Controller
     {
         private ApplicationDbContext db = new ApplicationDbContext();
 
-        // GET: Administrador/ReglaHigienes
-        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        // GET: ReglaHigienes
+        public ActionResult Index()
         {
-            ViewBag.CurrentSort = sortOrder;
-            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
-            ViewBag.DateSortParm = sortOrder == "Date" ? "date_desc" : "Date";
-
-            if (searchString != null)
-            {
-                page = 1;
-            }
-            else
-            {
-                searchString = currentFilter;
-            }
-
-            ViewBag.CurrentFilter = searchString;
-
-            var rHigiene = from s in db.Tb_ReglaHigiene
-                         select s;
-            if (!String.IsNullOrEmpty(searchString))
-            {
-                rHigiene = rHigiene.Where(s => s.Rhig_Registro.ToString().Contains(searchString)
-                                       || s.Rhig_Registro.ToString().Contains(searchString));
-            }
-            switch (sortOrder)
-            {
-                case "name_desc":
-                    rHigiene = rHigiene.OrderByDescending(s => s.Rhig_Registro.ToString());
-                    break;
-                default:  // Name ascending 
-                    rHigiene = rHigiene.OrderBy(s => s.Rhig_Registro.ToString());
-                    break;
-            }
-            int pageSize = 10;
-            int pageNumber = (page ?? 1);
-            return View(rHigiene.ToPagedList(pageNumber, pageSize));
-
+            var tb_ReglaHigiene = db.Tb_ReglaHigiene.Include(r => r.Empresa);
+            return View(tb_ReglaHigiene.ToList());
         }
 
-        // GET: Administrador/ReglaHigienes/Details/5
+        // GET: ReglaHigienes/Details/5
         public ActionResult Details(int? id)
         {
             if (id == null)
@@ -70,19 +38,22 @@ namespace Plenamente.Areas.Administrador.Controllers
             return View(reglaHigiene);
         }
 
-        // GET: Administrador/ReglaHigienes/Create
+        // GET: ReglaHigienes/Create
         public ActionResult Create()
         {
-            ViewBag.Empr_Nit = new SelectList(db.Tb_Empresa, "Empr_Nit", "Empr_Nom");
+            ApplicationDbContext entity = new ApplicationDbContext();
+
+            List<Empresa> listE = entity.Tb_Empresa.ToList();
+            ViewBag.EmpreList = new SelectList(listE, "Empr_Nit", "Empr_Nom");
             return View();
         }
 
-        // POST: Administrador/ReglaHigienes/Create
+        // POST: ReglaHigienes/Create
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "Rhig_Id,Rhig_Archivo,Empr_Nit,Rhig_Registro")] ReglaHigiene reglaHigiene)
+        public ActionResult Create([Bind(Include = "Rhig_Id,Rhig_Archivo,Rhig_Nom,Empr_Nit,Rhig_Registro")] ReglaHigiene reglaHigiene)
         {
             if (ModelState.IsValid)
             {
@@ -95,7 +66,7 @@ namespace Plenamente.Areas.Administrador.Controllers
             return View(reglaHigiene);
         }
 
-        // GET: Administrador/ReglaHigienes/Edit/5
+        // GET: ReglaHigienes/Edit/5
         public ActionResult Edit(int? id)
         {
             if (id == null)
@@ -111,12 +82,12 @@ namespace Plenamente.Areas.Administrador.Controllers
             return View(reglaHigiene);
         }
 
-        // POST: Administrador/ReglaHigienes/Edit/5
+        // POST: ReglaHigienes/Edit/5
         // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
         // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "Rhig_Id,Rhig_Archivo,Empr_Nit,Rhig_Registro")] ReglaHigiene reglaHigiene)
+        public ActionResult Edit([Bind(Include = "Rhig_Id,Rhig_Archivo,Rhig_Nom,Empr_Nit,Rhig_Registro")] ReglaHigiene reglaHigiene)
         {
             if (ModelState.IsValid)
             {
@@ -128,7 +99,7 @@ namespace Plenamente.Areas.Administrador.Controllers
             return View(reglaHigiene);
         }
 
-        // GET: Administrador/ReglaHigienes/Delete/5
+        // GET: ReglaHigienes/Delete/5
         public ActionResult Delete(int? id)
         {
             if (id == null)
@@ -143,7 +114,7 @@ namespace Plenamente.Areas.Administrador.Controllers
             return View(reglaHigiene);
         }
 
-        // POST: Administrador/ReglaHigienes/Delete/5
+        // POST: ReglaHigienes/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(int id)
@@ -161,6 +132,44 @@ namespace Plenamente.Areas.Administrador.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+        [HttpPost]
+        public ActionResult SaveRecord(Rhigiene rhigiene)
+        {
+            try
+            {
+                ApplicationDbContext entity = new ApplicationDbContext();
+                {
+                    ReglaHigiene rh = new ReglaHigiene();
+                    rh.Rhig_Nom = rhigiene.Rhig_Nom;
+                    rh.Rhig_Archivo = SaveToPhysicalLocation(rhigiene.Rhig_Archivo);
+                    rh.Rhig_Registro = rhigiene.Rhig_Registro;
+                    rh.Empr_Nit = rhigiene.Empr_Nit;
+
+                    entity.Tb_ReglaHigiene.Add(rh);
+                    entity.SaveChanges();
+
+                    int latest = rh.Rhig_Id;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return RedirectToAction("Create");
+        }
+        private string SaveToPhysicalLocation(HttpPostedFileBase file)
+        {
+            if (file.ContentLength > 0)
+            {
+                var fileName = Path.GetFileName(DateTime.Now.ToString("yyyyMMddHHmmss") + file.FileName);
+                var path = Path.Combine(Server.MapPath("~/Files"),fileName);
+                
+                file.SaveAs(path);
+                return fileName;
+            }
+            return string.Empty;
         }
     }
 }
