f4df0bcd1a5efbb568756cf82a37f4f2722221ce
Didiazhe <34241391+Didiazhe@users.noreply.github.com>
2019-06-01 18:56:12 -0500

    Adicion de dos entidades, Itemevidencia para relacion de empresa , item y evidencia (posiblemente falte algun atributo), itemevidenciaurl tabla de muchos a muchos (posiblemente falte un atributo)

---
+++ Plenamente/Plenamente/Models/ItemEvidenciaUrl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plenamente.Models
+{
+    public class ItemEvidenciaUrl
+    {
+        public int Id { get; set; }
+        public int ItemEvidencia_Id { get; set; }
+        public string Url { get; set; }
+        public virtual ItemEvidencia ItemEvidencia { get; set; }
+    }
+}
