60d3fabf6ec032d93518c6a43e2bacad1f386e25
Didiazhe <34241391+Didiazhe@users.noreply.github.com>
2019-06-03 14:46:08 -0500

    CargaArchivosEvidencia
    Se crea viewmodel para la referencia del archivo con el cumplimiento

---
+++ Plenamente/Plenamente/Models/ViewModel/EvidenciaCumplimientoViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Plenamente.Models.ViewModel
+{
+    public class EvidenciaCumplimientoViewModel
+    {
+       public Evidencia Evidencia { get; set; }    
+        public EvidenciaCumplimientoViewModel()
+        {            
+            Evidencia = new Evidencia();
+        }
+        [Display(Name = "Numero de documento")]
+        public int IdDocumento { get; set; }
+        [Display(Name = "Tipo de documento")]
+        public string TipoDocumento { get; set; }
+        [Display(Name = "Responsable")]
+        public string Responsable { get; set; }
+        [Display(Name = "Fecha")]
+        public int Fecha { get; set; }
+        [Display(Name = "Manual")]
+        public int Manual { get; set; }
+        [Display(Name = "Registro")]
+        public int Registro { get; set; }
+        [Required]
+        [Display(Name = "Archivo")]
+        public HttpPostedFileBase Archivo { get; set; }
+        public int IdCumplimiento { get; set; }
+    }
+}
