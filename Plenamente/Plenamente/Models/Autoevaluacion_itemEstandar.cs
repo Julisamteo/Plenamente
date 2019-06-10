5a433295dbc48167ce9064d84ee2f6ef7f440395
Andres Olarte <developer.laoz.396@gmail.com>
2019-06-10 10:58:57 -0500

    Se añade entidad autoevaluacion_itemestandar
    Se añade tabla muchos a muchos para autoevaluacion e itemestandar

---
+++ Plenamente/Plenamente/Models/Autoevaluacion_itemEstandar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Plenamente.Models
+{
+    public class Autoevaluacion_itemEstandar
+    {
+        public int Id { get; set; }
+        [ForeignKey("AutoEvaluacion")]
+        public int Id_autoevaluacion { get; set; }
+        [ForeignKey("ItemEstandar")]
+        public int Id_itemestandar { get; set; }
+        public bool Cumplimiento { get; set; }
+        public bool Estado { get; set; }
+        public double Value { get; set; }
+        public AutoEvaluacion AutoEvaluacion { get; set; }
+        public ItemEstandar ItemEstandar { get; set; }
+    }
+}
