72650eba5337e066e32ca7de529ec12f11356a73
Loko24 <epastor@sanmateo.edu.co>
2019-06-12 11:56:10 -0500

    Create users function active

--- Plenamente/Plenamente/Controllers/adminController.cs
+++ Plenamente/Plenamente/Controllers/adminController.cs
@@ -5,6 +5,7 @@ using PagedList;
 using Plenamente.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Validation;
 using System.Linq;
 using System.Net;
 using System.Web;
@@ -271,12 +272,28 @@ namespace Plenamente.Areas.Administrador.Controllers
                     return View(paramExpandedUserDTO);
                 }
             }
-            catch (Exception ex)
+            catch (DbEntityValidationException ex)
             {
-                ViewBag.Roles = GetAllRolesAsSelectList();
-                ModelState.AddModelError(string.Empty, "Error: " + ex);
-                return View("Create");
+                // Retrieve the error messages as a list of strings.
+                var errorMessages = ex.EntityValidationErrors
+                        .SelectMany(x => x.ValidationErrors)
+                        .Select(x => x.ErrorMessage);
+
+                // Join the list to a single string.
+                var fullErrorMessage = string.Join("; ", errorMessages);
+
+                // Combine the original exception message with the new one.
+                var exceptionMessage = string.Concat(ex.Message, " The validation errors are: ", fullErrorMessage);
+
+                // Throw a new DbEntityValidationException with the improved exception message.
+                throw new DbEntityValidationException(exceptionMessage, ex.EntityValidationErrors);
             }
+            //catch (Exception ex)
+            //{
+            //    ViewBag.Roles = GetAllRolesAsSelectList();
+            //    ModelState.AddModelError(string.Empty, "Error: " + ex);
+            //    return View("Create");
+            //}
         }
         #endregion
 
