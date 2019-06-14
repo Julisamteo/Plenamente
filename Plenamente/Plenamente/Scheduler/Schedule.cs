73d1437adc98d423e1f15a7769305c876a24a429
Evilio Polo M <eviliopolo79@gmail.com>
2019-06-13 23:38:38 -0500

    Conjunto de clases para generar una programacion de eventos sencillos o repetibles
    Se crea un conjunto de clase que permiten generar una programacion de eventos en el tiempo

---
+++ Plenamente/Plenamente/Scheduler/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Plenamente.Scheduler
+{
+    public abstract class Schedule
+    {
+        public TimeSpan TimeOfDay { get; set; }
+
+        public string Name { get; set; }
+
+        public abstract bool OccursOnDate(DateTime date);
+    }
+}
