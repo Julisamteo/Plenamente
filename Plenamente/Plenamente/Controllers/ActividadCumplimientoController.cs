c615201c69fc60bdaa85dbeed3cbb263ba7541c9
eviliopolo <eviliopolo79@gmail.com>
2019-06-15 11:00:10 -0500

    nuevo controlador actividades de cumpliento
    Se agrega nuevo controlador para toda la gestion de actividades de cumpliento

---
+++ Plenamente/Plenamente/Controllers/ActividadCumplimientoController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Plenamente.Controllers
+{
+    public class ActividadCumplimientoController : Controller
+    {
+        // GET: ActividadCumplimiento
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: ActividadCumplimiento/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: ActividadCumplimiento/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ActividadCumplimiento/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ActividadCumplimiento/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: ActividadCumplimiento/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: ActividadCumplimiento/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: ActividadCumplimiento/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Plenamente/Plenamente/Models/ViewModel/ViewModelActividadCumplimiento.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plenamente.Models.ViewModel
+{
+    public class ViewModelActividadCumplimiento
+    {
+    }
+}
