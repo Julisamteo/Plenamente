2ce517f7cb41d3083c0f73ea3c6eba277faa92a6
Didiazhe <34241391+Didiazhe@users.noreply.github.com>
2019-06-15 13:53:31 -0500

    Actualizacion plan de trabajo
    Se añade viste que relacion plan de trabajo con actividad y a la vez relaciona con responsable

--- Plenamente/Plenamente/Models/ViewModel/PlandetrabajoActividadesViewModel.cs
+++ Plenamente/Plenamente/Models/ViewModel/PlandetrabajoActividadesViewModel.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
 namespace Plenamente.Models.ViewModel
 {
     public class PlandetrabajoActividadesViewModel
-    {     
+    {
         public int IdPlantTrabajo { get; set; }
+        [Display(Name = "Nombre de plan de trabajo")]
         public string NombrePlanTrabajo { get; set; }
-        public string IdUser { get; set; }        
+        [Display(Name = "Usuarios")]
+        public string IdUser { get; set; }       
+        [Display(Name = "Actividad")]
         public int IdActiCumplimiento { get; set; }
+        [Display(Name = "Descripcion actividad")]
         public string DescripcionCumplimiento { get; set; }
     }
 }
