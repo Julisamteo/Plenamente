c1a10276c09c8aab45632d4ba40dea2c61acdbcf
Didiazhe <34241391+Didiazhe@users.noreply.github.com>
2019-06-15 10:57:40 -0500

    Migracion acticumplimiento
    Se cambia referencia de tablas , se continua trabajando en creacion de plan de trabajo

---
+++ Plenamente/Plenamente/Models/ViewModel/PlandetrabajoActividadesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Plenamente.Models.ViewModel
+{
+    public class PlandetrabajoActividadesViewModel
+    {      
+        public int IdPlantTrabajo { get; set; }
+        public string NombrePlanTrabajo { get; set; }
+        public string IdUser { get; set; }
+        public int IdActiCumplimiento { get; set; }
+        public string NombreCumplimiento { get; set; }
+    }
+}
