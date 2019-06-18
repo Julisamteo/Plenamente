eaaf2e8a2f3b33a4428a6f4d8431fbeed5a8103d
rodolforodriguez <40470633+rodolforodriguez@users.noreply.github.com>
2019-06-18 18:28:37 -0500

    se agregan columnas a la tabla acticumplimiento
    las columnas nrorepeticiones y diassemana

--- Plenamente/Plenamente/Models/ActiCumplimiento.cs
+++ Plenamente/Plenamente/Models/ActiCumplimiento.cs
@@ -49,9 +49,10 @@ namespace Plenamente.Models
         [ForeignKey("Frecuencia")]
         public int Frec_Id { get; set; }
         public Frecuencia Frecuencia { get; set; }
+        public string DiasSemana { get; set; }
+        public int Repeticiones { get; set; }
 
 
-     
         // Permite que Acummes acceda a la data
         public ICollection<AcumMes> AcumMes { get; set; }      
         public ICollection<UsuariosPlandetrabajo> Usersplandetrabajo { get; set; }
