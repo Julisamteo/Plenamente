01ddd9b2da155e805f69eb769172cdf890a5c271
Carlos <Carlos@DESKTOP-213EP4J>
2019-06-21 11:28:25 -0500

    Modal y Estilos Autoev

---
+++ Plenamente/Plenamente/Migrations/201906211541272_qeww.cs
@@ -0,0 +1,18 @@
+namespace Plenamente.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class qeww : DbMigration
+    {
+        public override void Up()
+        {
+
+        }
+        
+        public override void Down()
+        {
+
+        }
+    }
+}
