ab961c4602b8fdc2c1129cac7d95b2f8294c45a5
Carlos <Carlos@DESKTOP-213EP4J>
2019-08-08 17:03:57 -0500

    Modal En plan de trabajo

--- Plenamente/Plenamente/Controllers/PlandeTrabajoController.cs
+++ Plenamente/Plenamente/Controllers/PlandeTrabajoController.cs
@@ -448,7 +448,123 @@ namespace Plenamente.Controllers
 
 		}
 
-		protected override void Dispose(bool disposing)
+        public ActionResult IndexM(int pagina = 1)
+        {
+            int _TotalRegistros = 0;
+            var tb_PlandeTrabajo = db.Tb_PlandeTrabajo.Where(p => p.Emp_Id == AccountData.NitEmpresa).ToList();
+            _TotalRegistros = tb_PlandeTrabajo.Count();
+            tb_PlandeTrabajo = tb_PlandeTrabajo.Skip((pagina - 1) * _RegistrosPorPagina)
+                                               .Take(_RegistrosPorPagina)
+                                               .ToList();
+            int _TotalPaginas = (int)Math.Ceiling((double)_TotalRegistros / _RegistrosPorPagina);
+            _PaginadorCustomers = new PaginadorGenerico<PlandeTrabajo>()
+            {
+                RegistrosPorPagina = _RegistrosPorPagina,
+                TotalRegistros = _TotalRegistros,
+                TotalPaginas = _TotalPaginas,
+                PaginaActual = pagina,
+                Resultado = tb_PlandeTrabajo
+            };
+            return View(_PaginadorCustomers);
+        }
+        /// <summary>
+		/// lista y pagina las actividades que tiene un plan de trabajo dependiendo de la pagina 
+		/// </summary>
+		/// <param name="IdPlantTrabajo">recibe el id del plan de trabajo</param>
+		/// <param name="pagina">recibe la pagina actual en la que se esta paginando</param>
+		/// <returns>retorna un objeto de tipo paginadorcustomer que contiene la paginacion y el objeto a paginar</returns>
+		public ActionResult Actividadesplantra(int IdPlantTrabajo, int pagina = 1)
+        {
+            var plantrabajo = db.Tb_PlandeTrabajo.Find(IdPlantTrabajo);
+            ViewBag.users = new SelectList(db.Users.Where(c => c.Empr_Nit == AccountData.NitEmpresa), "Id", "Pers_Nom1");
+            var actividadesEmpresa = db.Tb_ActiCumplimiento.Where(c => c.Empr_Nit == AccountData.NitEmpresa).ToList();
+            List<ActiCumplimiento> actiCumplimientoSinAsignar = new List<ActiCumplimiento>();
+            List<ActividadesAsignadasPlanDeTrabajoViewModel> actiCumplimientoAsignados = new List<ActividadesAsignadasPlanDeTrabajoViewModel>();
+            foreach (var item in actividadesEmpresa)
+            {
+                var useractividadpt = db.Tb_UsersPlandeTrabajo.Where(c => c.Acum_Id == item.Acum_Id).ToList();
+                if (useractividadpt.Count <= 0)
+                {
+                    actiCumplimientoSinAsignar.Add(item);
+                }
+                else
+                {
+                    var cumplimientoPlanDetrabajo = db.Tb_UsersPlandeTrabajo.Where(c => c.Plat_Id == IdPlantTrabajo && c.Acum_Id == item.Acum_Id).ToList();
+                    if (cumplimientoPlanDetrabajo.Count > 0)
+                    {
+
+                        var user = db.Tb_UsersPlandeTrabajo.First(c => c.Acum_Id == item.Acum_Id);
+                        var nombre = db.Users.Find(user.Id);
+
+                        ActividadesAsignadasPlanDeTrabajoViewModel temp = new ActividadesAsignadasPlanDeTrabajoViewModel
+                        {
+                            IdUserPlanDeTrabajoActividad = user.Uspl_Id,
+                            NombreUser = nombre.Pers_Nom1 + " " + nombre.Pers_Apel1,
+                            IdPlantTrabajo = plantrabajo.Plat_Id,
+                            IdActiCumplimiento = item.Acum_Id,
+                            DescripcionCumplimiento = item.Acum_Desc,
+                            NombrePlanTrabajo = plantrabajo.Plat_Nom
+
+                        };
+                        actiCumplimientoAsignados.Add(temp);
+                    }
+
+                }
+            }
+            ViewBag.actividades = new SelectList(actiCumplimientoSinAsignar, "Acum_Id", "Acum_Desc");
+            PlandetrabajoActividadesViewModel plandetrabajoActividades = new PlandetrabajoActividadesViewModel
+            {
+                NombrePlanTrabajo = plantrabajo.Plat_Nom,
+                IdPlantTrabajo = plantrabajo.Plat_Id
+            };
+            int _TotalRegistros = 0;
+            _TotalRegistros = actiCumplimientoAsignados.Count();
+            actiCumplimientoAsignados = actiCumplimientoAsignados.Skip((pagina - 1) * _RegistrosPorPaginaActividades)
+                                               .Take(_RegistrosPorPaginaActividades)
+                                               .ToList();
+            int _TotalPaginas = (int)Math.Ceiling((double)_TotalRegistros / _RegistrosPorPaginaActividades);
+            _PaginadorCustomersActividades = new PaginadorGenerico<ActividadesAsignadasPlanDeTrabajoViewModel>()
+            {
+                RegistrosPorPagina = _RegistrosPorPaginaActividades,
+                TotalRegistros = _TotalRegistros,
+                TotalPaginas = _TotalPaginas,
+                PaginaActual = pagina,
+                Resultado = actiCumplimientoAsignados
+            };
+
+            ViewBag.actividadesAsignadas = _PaginadorCustomersActividades;
+            return View(plandetrabajoActividades);
+        }
+        /// <summary>
+        /// relaciona un usuario a una actividad de un plan de trabajo
+        /// </summary>
+        /// <param name="model">recibe un objeto de tipo PlandetrabajoActividadesViewModel para lelacionarlo a un usuario</param>
+        /// <returns>redirecciona a actividadesplantrabajo con el id del plan de trabajo</returns>
+        [HttpPost]
+
+        public ActionResult Actividadesplantra([Bind(Include = "IdPlantTrabajo,IdActiCumplimiento,IdUser")]PlandetrabajoActividadesViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+                UsuariosPlandetrabajo user = new UsuariosPlandetrabajo
+                {
+                    Acum_Id = model.IdActiCumplimiento,
+                    Plat_Id = model.IdPlantTrabajo,
+                    Emp_Id = AccountData.NitEmpresa,
+                    Id = model.IdUser
+                };
+                PlandeTrabajo plandeTrabajo = db.Tb_PlandeTrabajo.Find(model.IdPlantTrabajo);
+                plandeTrabajo.FechaActualizacion = DateTime.Now;
+                db.Entry(plandeTrabajo).State = EntityState.Modified;
+                db.Tb_UsersPlandeTrabajo.Add(user);
+                db.SaveChanges();
+            }
+
+            return RedirectToAction("ActividadesPlanTrabajo", new { model.IdPlantTrabajo });
+        }
+
+        protected override void Dispose(bool disposing)
 		{
 			if (disposing)
 			{
